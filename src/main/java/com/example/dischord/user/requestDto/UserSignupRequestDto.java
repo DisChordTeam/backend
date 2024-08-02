7e2c843615dd0a9eee7cb1beecefb50608b970c0
7zrv <4014133@gmail.com>
2024-08-02 16:34:44 +0900

    feat: 로그인 실패 response Json기능 추가

--- src/main/java/com/example/dischord/user/requestDto/UserSignupRequestDto.java
+++ src/main/java/com/example/dischord/user/requestDto/UserSignupRequestDto.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class UserSignupRequestDto {
